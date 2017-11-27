75d201f1517739a97d1603fe9ee9e03961a38bec
ewadkins <ewadkins83@gmail.com>
2017-11-26 19:14:54 -0500

    Added embeddings loader

--- src/main1.py
+++ src/main1.py
@@ -1,9 +1,5 @@
 import utils
 
-print 'Loading corpus..'
-question_map = utils.load_corpus('../data/text_tokenized.txt')
-print len(question_map)
-
 print 'Loading training samples..'
 training_samples = utils.load_samples('../data/train_random.txt')
 print len(training_samples)
@@ -15,6 +11,14 @@ print len(dev_samples)
 print 'Loading test samples..'
 test_samples = utils.load_samples('../data/test.txt')
 print len(test_samples)
+
+print 'Loading corpus..'
+question_map = utils.load_corpus('../data/text_tokenized.txt')
+print len(question_map)
+
+print 'Loading embeddings..'
+embedding_map = utils.load_embeddings('../data/vectors_pruned.200.txt')
+print len(embedding_map)
 
 print
 
--- src/utils.py
+++ src/utils.py
@@ -21,8 +21,17 @@ def load_samples(filepath):
         return map(lambda x: Sample(*map(lambda (i, y):
                                          y.split() if i != 0 else y, enumerate(x.split('\t')))), samples)
 
+# Returns a dictionary mapping question id's to their (title, body)
 def load_corpus(filepath):
     with open(filepath, 'r') as f:
         corpus = [line.strip() for line in f.readlines()]
         corpus = map(lambda x: x.split('\t'), corpus)
         return {x[0]: tuple(x[1:]) for x in corpus}
+    
+# Returns a dictionary mapping words to their 200-dimension pre-trained word embeddings
+def load_embeddings(filepath):
+    with open(filepath, 'r') as f:
+        embeddings = [line.strip() for line in f.readlines()]
+        embeddings = map(lambda x: map(lambda (i, y):
+                                       float(y) if i != 0 else y, enumerate(x.split())), embeddings)
+        return {x[0]: tuple(x[1:]) for x in embeddings}
