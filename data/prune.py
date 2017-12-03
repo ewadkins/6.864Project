057fdabb232d60fe0511ca5bbd7b1abb408245a2
ewadkins <ewadkins83@gmail.com>
2017-12-03 00:22:04 -0500

    Fixed pruning

--- data/prune.py
+++ data/prune.py
@@ -8,12 +8,12 @@ for fp in relevant:
     with open(fp, 'r') as f:
         for line in f:
             tmp = map(lambda x: x.split(), filter(bool, line.split('\t')[1:]))
-            relevant_set |= set([item for sublist in tmp for item in sublist])
+            relevant_set |= set([word.lower() for sublist in tmp for word in sublist])
 
             
 with open(filepath, 'r') as f:
     with open(target, 'a') as tf:
         for line in f:
-            word = line.split()[0]
+            word = line.split()[0].lower()
             if word in relevant_set:
                 tf.write(line.strip() + '\n')
